d4beb54e64a6bd074dd38c678dcfebdf7cb08c26
unknown <liubomyr.lypetskyi@gmail.com>
2022-10-01 19:07:00 +0300

    added struct & enum exercises

---
+++ enum_lesson/target/debug/deps/enum_lesson.d
@@ -0,0 +1,5 @@
+D:\Projects\RUST\RUST_lessons\enum_lesson\target\debug\deps\enum_lesson.exe: src\main.rs
+
+D:\Projects\RUST\RUST_lessons\enum_lesson\target\debug\deps\enum_lesson.d: src\main.rs
+
+src\main.rs:
---
+++ enum_lesson/target/debug/enum_lesson.d
@@ -0,0 +1,1 @@
+D:\Projects\RUST\RUST_lessons\enum_lesson\target\debug\enum_lesson.exe: D:\Projects\RUST\RUST_lessons\enum_lesson\src\main.rs
